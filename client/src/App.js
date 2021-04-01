9b39a43a4ad2005d5861fbfaaa18218586649733
amrutsabale <samrut355@gmail.com>
2021-04-01 11:32:43 +0530

    Implemented react routing setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Landing from "./components/layouts/Landing";
 import NavBar from "./components/layouts/NavBar";
 import "./App.css";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 function App() {
   return (
-    <>
+    <Router>
       <NavBar />
-      <Landing />
-    </>
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
+    </Router>
   );
 }
 
