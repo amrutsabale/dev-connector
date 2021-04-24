329d3a87ab6672fa82f88517f3f9313e03beaf4d
amrutsabale <samrut355@gmail.com>
2021-04-24 19:44:04 +0530

    Implemented addLike & removeLike

---
+++ client/src/actions/postActions.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { setAlert } from "./alertActions";
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
+
+export const getPosts = () => async (dispatch) => {
+  try {
+    const res = await axios.get("/api/posts");
+
+    dispatch({
+      type: GET_POSTS,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+export const addLike = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/like/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id: postId, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+export const removeLike = (postId) => async (dispatch) => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id: postId, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+export const deletePost = () => {};
--- client/src/components/layouts/NavBar.js
+++ client/src/components/layouts/NavBar.js
@@ -10,6 +10,9 @@ const NavBar = ({ logout, auth: { isAuthenticated, loading } }) => {
         <Link to="/profiles">Developers</Link>
       </li>
       <li>
+        <Link to="/posts">Posts</Link>
+      </li>
+      <li>
         <Link to="/dashboard">
           <i className="fas fa-user"></i>{" "}
           <span className="hide-sm">Dashboard</span>
