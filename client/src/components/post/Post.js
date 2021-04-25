2853fb2d8a17a3bc056eb4e72c228ba985232509
amrutsabale <samrut355@gmail.com>
2021-04-25 12:41:12 +0530

    Implemeneted get a single Post

---
+++ client/src/components/post/Post.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Spinner from "../layouts/Spinner";
+import { getPost } from "../../actions/postActions";
+import PostItem from "../posts/PostItem";
+import { Link } from "react-router-dom";
+
+const Post = ({ getPost, post: { post, loading }, match }) => {
+  useEffect(() => {
+    getPost(match.params.id);
+  }, [getPost, match.params.id]);
+
+  return loading || post === null ? (
+    <Spinner />
+  ) : (
+    <>
+      <Link to="/posts" className="btn">
+        Back To Posts
+      </Link>
+      <PostItem post={post} showActions={false} />
+    </>
+  );
+};
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  post: state.post,
+});
+
+export default connect(mapStateToProps, { getPost })(Post);
